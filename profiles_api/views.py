6cd77bf92b256b0fd5ae4315e747178505a7d51a
Rami <rami.live@hotmail.com>
2021-01-11 20:40:38 +0200

    Added user profile with permissions and authToken and enabled search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,11 @@ from rest_framework import status
 from rest_framework import viewsets
 from profiles_api import serializers
 
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+from rest_framework import filters
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -99,3 +104,13 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
